2c9285e653bfd12c426e275f0bd878e439326492
Ahmed Mohamed Abozeid <157320734+Bozeid55@users.noreply.github.com>
2026-04-02 04:11:11 +0200

    Create C - Driving Range.cpp

---
+++ Juniors Phase 2 Session #9  DSU & MST [Part1]/C - Driving Range.cpp
@@ -0,0 +1,103 @@
+// وَكَانَ فَضْلُ اللَّهِ عَلَيْكَ عَظِيمًا♡
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define input(vec) for (auto& i : vec) cin >> i
+#define inputone(vec , x) for (int i = 1 ; i <= x ; i++) cin >> vec[i]
+#define msb(x) 64 - ll(__builtin_clzll(x))
+#define ABo_ZeiD ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+const ll N = 1e5 + 5 , w = 1e6 + 5 , oo = 1e9;
+
+int dx[] = {0 , 0  , 1 , -1} ,
+    dy[] = {1 , -1 , 0 , 0};
+
+struct DSU {
+    vector <int> parent , group; int sz;
+
+    DSU (int n) {
+        parent = vector <int> (n + 1); sz = n;
+        group = vector <int> (n + 1 , 1);
+        iota(all(parent) , 0);
+    }
+
+    int getLeader (int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = getLeader(parent[x]);
+    }
+
+    bool Merge (int x, int y) {
+        int Leader1 = getLeader(x), Leader2 = getLeader(y);
+        if (Leader1 == Leader2) return 0;
+
+        if (group[Leader1] > group[Leader2]) swap(Leader1, Leader2);
+
+        parent[Leader1] = Leader2;
+        group[Leader2] += group[Leader1];
+
+        return 1;
+    }
+
+    bool sameGroup (int x, int y) {
+        return getLeader(x) == getLeader(y);
+    }
+
+    int getSize (int x) {
+        return group[getLeader(x)];
+    }
+};
+
+void solve () {
+    int n , m;
+
+    while (cin >> n >> m) {
+        if (!n && !m) return;
+
+        int  a , b , c;
+        vector <array<int , 4>> mst;
+        DSU dsu(n); int mx = 0;
+
+        for (int i = 1 ; i <= m ; i++) {
+            cin >> a >> b >> c;
+            mst.push_back({c , a , b , i});
+        }
+
+        sort(all(mst));
+
+        int ck = 0;
+        for (int i = 0 ; i < m ; i++) {
+            auto [x, l, u, k] = mst[i];
+
+            if (dsu.Merge(l , u)) {
+                mx = max(mx , x); ck++;
+            }
+        }
+
+        if (ck != n - 1) {
+            cout << "IMPOSSIBLE" << '\n'; continue;
+        }
+
+        cout << mx << '\n';
+    }
+
+}
+
+int main() {
+    ABo_ZeiD; // i'm backkkkkk;
+
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+#endif
+
+    int t = 1;
+    //cin >> t;
+    for (int i = 1 ; i <= t ; i++) {
+        // cout << "Case #" << i << " : ";
+        solve();
+        // cout << '\n';
+    }
+
+
+    return 0;
+}
